cd679d16785eb08381fb9aef107fa743ec10bf6d
Nhlchrs <nhlchaurasia1234@gmail.com>
2020-10-23 15:31:21 +0530

    user Forms

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, {useState} from 'react'
-
+import './App.css'
+import Pass from './Pass'
 function App() {
 
   const [input, setInput] =useState('')
   const [textArea , setTextarea]= useState('')
-  const [selected, setSelected] = useState('grapefruit')
-  const [checkone, setCheckOne] = useState('')
-  const [checktwo, setChecktwo] = useState('')
-  const [checkthree, setCheckthree] = useState('')
+  const [selected, setSelected] = useState('BCA')
+  const [check, setCheck] = useState([])
+  const [radio , setRadio] = useState('');
+  const [user, setUser] = useState([{id:Math.random(), name:'input', bio:'textArea', course:'selected', hobbies:['check'], gender:'Male'}])
+  
+  
+  const handleChange=(e)=>{
+    if(e.target.checked===true){
+      setCheck([...check, e.target.value])
+    }
+  }
+
   const handleSubmit=(e)=>{
     e.preventDefault();
-    alert(input +" "+ textArea + " " + selected )
+    setUser([...user, {id:Math.random(), name:input, bio:textArea, course:selected, hobbies:check, gender:radio}])
+    
   }
+console.log(user)
   return (
-    <div>
+    <div >
+      <div className="form__app">
       <form onSubmit={handleSubmit}>
   <label>
     Name:
@@ -25,40 +37,37 @@ function App() {
   <textarea value={textArea} onChange={(e)=>setTextarea(e.target.value)}>
   </textarea>
 <br/>
-<label>Fruits: </label>
+<label>Couses: </label>
 <select value={selected} onChange={(e)=>setSelected(e.target.value)}>
-  <option value="grapefruit">Grapefruit</option>
-  <option value="lime">Lime</option>
-  <option selected value="coconut">Coconut</option>
-  <option value="mango">Mango</option>
+  <option value="BCA">BCA</option>
+  <option value="BA">BA</option>
+  <option value="MBA">MBA</option>
+  <option value="CA">CA</option>
 </select>
 <br/>
 <label>
           hobbies:</label>  <br/>
           <label>Cricket : </label>
-          <input type="checkbox"/>
+          <input value="cricket" name="cricket" onChange={handleChange} type="checkbox"/>
           <label>Football : </label>
-
-            <input value="" type="checkbox"/>
+            <input value="Football"  onChange={handleChange} type="checkbox"/>
           <label>Soccer : </label>
-
-            <input type="checkbox" />
-            
-
-
+            <input value="Soccer" onChange={handleChange} type="checkbox" />
 <br/>
 <label>
           Gender:</label><label>Male : 
-          <input
-        name="gender"
-            type="radio"/>
+          <input value="Male" onChange={(e)=>setRadio(e.target.value)} name="gender" type="radio"/>
             </label>
             <label>Female : 
-          <input name="gender"
-            type="radio"/></label>
+          <input value="Female" onChange={(e)=>setRadio(e.target.value)} name="gender" type="radio"/>
+          
+          </label>
 <br/>
   <input type="submit" value="Submit" />
 </form>
+</div>
+<Pass user={user}/>
+
     </div>
   )
 }
--- src/Pass.js
+++ src/Pass.js
@@ -1,16 +1,48 @@
-import React, { Component } from 'react'
-
-export default class Pass extends Component {
-    constructor(props){
-        super(props);
-        
+import React, {useState, useEffect} from 'react'
+import './App.css'
+function Pass({user}) {
+    const [filter, setFilter] = useState("ALL")
+  useEffect(()=>{
+    if(filter==='Male'){
+        let newUser = user.filter(item=>item.gender==='Male' ? [item.id, item.bio] : item)
+        console.log(newUser)
     }
-   
-    render() {
-        return (
-            <div>
-                <h1>Propsssss</h1>
+    else if(filter==='Female'){
+        let newUsers = user.filter(item=>item.gender===filter? [item.id, item.bio] : 'item')
+        console.log(newUsers)
+    }
+    else{
+        alert(filter+"all")
+    }
+  },[filter,user])
+  console.log(filter)
+    return (
+        <div >
+            {user.length > 0 ? (
+                <div style={{marginTop:"10px"}}>
+                <select onChange={(e)=>setFilter(e.target.value)} style={{width:"100%"}}>
+                <option value="ALL">All</option>
+                <option value="Male">Male</option>
+                <option value="Female" >Female</option>
+              </select>
+            
+            {user.map((item,index)=>(
+                <div key={index} className="form_padding">
+                    <h3>{item.name}</h3>
+                    <p>Bio : {item.bio}</p>
+            <p>Gender : {item.gender}</p>
+            <ul>
+                {item.hobbies.map((item,index)=>(
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+                <h4>Course : {item.course}</h4>
+                </div>
+            ))}
             </div>
-        )
-    }
+            ) : (<h1>No Users Records</h1>)}
+        </div>
+    )
 }
+
+export default Pass
